15e18ff2f486d364bb68fe0fd22b102fa08e752c
Shubham <shubham.kumar@myfindoc.com>
2026-05-14 13:40:14 +0530

    LOGIN LOGOUT FLOW

--- pt_project/urls.py
+++ pt_project/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/paper-trading/", permanent=False)),
     path("admin/", admin.site.urls),
     path("paper-trading/", include("paper_trading.urls")),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
 ]
 
 if settings.DEBUG:
